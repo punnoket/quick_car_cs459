41aa08f7c01d4b0c3bb98e259c6fc71d8a1134aa
Punnnnn ' n <Punnnnn ' n>
2017-04-26 19:40:32 +0700

    connect

--- quick_car/quick_car/settings.py
+++ quick_car/quick_car/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'quick_app'
 ]
 
 MIDDLEWARE_CLASSES = [
--- quick_car/quick_car/urls.py
+++ quick_car/quick_car/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from quick_app import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^map', views.map, name='map')
 ]
