a73241d212aaf1ced68f471d4454443e5c2b0f26
Punnnnn ' n <Punnnnn ' n>
2017-04-30 01:22:05 +0700

    add requirment

--- quick_car/quick_car/settings.py
+++ quick_car/quick_car/settings.py
@@ -78,8 +78,17 @@ WSGI_APPLICATION = 'quick_car.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(PROJECT_ROOT, 'db.sqlite3'),
     }
+}
+
+
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 
