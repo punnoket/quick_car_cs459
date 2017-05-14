e1c301612458daca8da27863095b94b4dcb30e9a
gminemini <mine_wongteeravit@hotmail.com>
2017-05-14 17:26:32 +0700

    test fastpi6

--- quick_car/quick_car/settings.py
+++ quick_car/quick_car/settings.py
@@ -121,7 +121,7 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
