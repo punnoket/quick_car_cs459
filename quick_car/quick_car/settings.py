7d2b464afe0b70525e743be097bd8c3d2de2d481
gminemini <mine_wongteeravit@hotmail.com>
2017-05-14 17:20:57 +0700

    test fastpic5

--- quick_car/quick_car/settings.py
+++ quick_car/quick_car/settings.py
@@ -121,10 +121,9 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
-# Extra places for collectstatic to find static files.
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
